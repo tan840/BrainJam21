ae5b7994b1c4da6ba9be122fcf57ef8c143c951e
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2021-03-28 20:59:25 +0600

    Nav Mesh Setup for the enemy zombie

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float range = 3f;
+    [SerializeField] private float timeBetweenAttacks = 1f;
+
+    [SerializeField] private Animator anim;
+    private GameObject player;
+    private bool playerInRange;
+    [SerializeField] private BoxCollider[] weaponColliders;
     void Start()
     {
-        
+        weaponColliders = GetComponentsInChildren<BoxCollider>();
+        player = GameManager.instance.Player;
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, player.transform.position) < range)
+        {
+            playerInRange = true;
+            StartCoroutine(Attack());
+        }else
+        {
+            playerInRange = false;
+        }
+        print(playerInRange);
+    }
+    IEnumerator Attack()
+    {
+        if (playerInRange && !GameManager.instance.GameOver)
+        {
+            anim.Play("Zombie Attack");
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+        yield return null;
     }
 }
